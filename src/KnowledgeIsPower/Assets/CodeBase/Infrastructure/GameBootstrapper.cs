4e66d84dbadccf896677c872377da42823fcc144
roman_gorovtsov <feeling.digital.edu@gmail.com>
2021-06-28 20:40:52 +0300

    Added Scene Loader and Initial Scene

--- src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/GameBootstrapper.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/GameBootstrapper.cs
@@ -2,13 +2,13 @@
 
 namespace CodeBase.Infrastructure
 {
-  public class GameBootstrapper : MonoBehaviour
+  public class GameBootstrapper : MonoBehaviour, ICoroutineRunner
   {
     private Game _game;
 
     private void Awake()
     {
-      _game = new Game();
+      _game = new Game(this);
       _game.StateMachine.Enter<BootstrapState>();
 
       DontDestroyOnLoad(this);
