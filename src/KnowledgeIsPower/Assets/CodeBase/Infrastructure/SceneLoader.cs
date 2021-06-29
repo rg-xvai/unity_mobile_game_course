a48fcc43aaa29a514ce5a168cbd9667bda655a58
roman_gorovtsov <feeling.digital.edu@gmail.com>
2021-06-29 18:40:57 +0300

    Added a check for an active scene in the SceneLoader

--- src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/SceneLoader.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/SceneLoader.cs
@@ -14,9 +14,15 @@ namespace CodeBase.Infrastructure
     public void Load(string name, Action onLoaded = null) => _coroutineRunner.StartCoroutine((LoadScene(name, onLoaded)));
   
 
-    private IEnumerator LoadScene(string name, Action onLoaded = null)
-    { 
-      AsyncOperation waitNextScene = SceneManager.LoadSceneAsync(name);
+    private IEnumerator LoadScene(string nextScene, Action onLoaded = null)
+    {
+      if (SceneManager.GetActiveScene().name == nextScene)
+      {
+        onLoaded?.Invoke();
+        yield break;
+      }
+      
+      AsyncOperation waitNextScene = SceneManager.LoadSceneAsync(nextScene);
 
       while (!waitNextScene.isDone)
         yield return null;
