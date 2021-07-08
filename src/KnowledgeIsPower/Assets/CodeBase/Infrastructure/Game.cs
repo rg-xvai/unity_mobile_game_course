0a63718bdfbcb47a95a4d2bb637a780979651b67
Sergei Artemyev <artemyev-sergei@yandex.ru>
2021-07-08 11:20:50 +0300

    Use custom DI and remove duplicate LoadingCurtain

--- src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/Game.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/Game.cs
@@ -1,16 +1,16 @@
-using CodeBase.Services.Input;
-using UnityEngine;
+using CodeBase.Infrastructure.Services;
+using CodeBase.Infrastructure.States;
+using CodeBase.Logic;
 
 namespace CodeBase.Infrastructure
 {
   public class Game
   {
-    public static IInputService InputService;
     public GameStateMachine StateMachine;
 
     public Game(ICoroutineRunner coroutineRunner, LoadingCurtain curtain)
     {
-      StateMachine = new GameStateMachine(new SceneLoader(coroutineRunner), curtain);
+      StateMachine = new GameStateMachine(new SceneLoader(coroutineRunner), curtain, AllServices.Container);
     }
   }
 }
---
+++ src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/States/BootstrapState.cs
@@ -0,0 +1,51 @@
+﻿using CodeBase.Infrastructure.AssetManagement;
+using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services;
+using CodeBase.Services.Input;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure.States
+{
+  public class BootstrapState : IState
+  {
+    private const string Initial = "Initial";
+    private readonly GameStateMachine _stateMachine;
+    private readonly SceneLoader _sceneLoader;
+    private readonly AllServices _services;
+
+    public BootstrapState(GameStateMachine stateMachine, SceneLoader sceneLoader, AllServices services)
+    {
+      _stateMachine = stateMachine;
+      _sceneLoader = sceneLoader;
+      _services = services;
+
+      RegisterServices();
+    }
+
+    public void Enter()
+    {
+      _sceneLoader.Load(Initial, onLoaded: EnterLoadLevel);
+    }
+
+    public void Exit()
+    {
+    }
+
+    private void EnterLoadLevel() => _stateMachine.Enter<LoadLevelState, string>("Main");
+
+    private void RegisterServices()
+    {
+      _services.RegisterSingle<IInputService>(InputService());
+      _services.RegisterSingle<IAssets>(new AssetProvider());
+      _services.RegisterSingle<IGameFactory>(new GameFactory(_services.Single<IAssets>()));
+    }
+
+    private static IInputService InputService()
+    {
+      if (Application.isEditor)
+        return new StandaloneInputService();
+      else
+        return new MobileInputService();
+    }
+  }
+}
