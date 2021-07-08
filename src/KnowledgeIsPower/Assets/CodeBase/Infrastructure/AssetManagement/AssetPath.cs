a9ab370646438b179ba4ebaf3f6b8ad6e50facd8
Sergei Artemyev <artemyev-sergei@yandex.ru>
2021-07-08 11:17:15 +0300

    Add AssetProvider

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/AssetManagement/AssetPath.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeBase.Infrastructure.AssetManagement
+{
+  public static class AssetPath
+  {
+    public const string HeroPath = "Hero/hero";
+    public const string HudPath = "Hud/Hud";
+  }
+}
