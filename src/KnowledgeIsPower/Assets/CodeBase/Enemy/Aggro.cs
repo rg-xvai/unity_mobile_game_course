ae80727778dcf143409e09c23c31e6dbe06bcd74
roman_gorovtsov <feeling.digital.edu@gmail.com>
2021-07-21 23:48:15 +0300

    Added Enemy

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Enemy/Aggro.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+  public class Aggro: MonoBehaviour
+  {
+    public Follow Follow;
+    public TriggerObserver TriggerObserver;
+    public float Cooldown;
+    private Coroutine _aggroCoroutine;
+    private bool _hasAggroTarget;
+
+    private void Start()
+    {
+      TriggerObserver.TriggerEnter += TriggerEnter; 
+      TriggerObserver.TriggerExit += TriggerExit;
+      SwitchFollowOff();
+    }
+    
+    private void TriggerEnter(Collider obj)
+    {
+      if(!_hasAggroTarget)
+      {
+        _hasAggroTarget = true;
+        StopAggroCoroutine();
+        SwitchFollowOn();
+      }
+
+    }
+
+    private void TriggerExit(Collider obj)
+    {
+      if (_hasAggroTarget)
+      {
+        _hasAggroTarget = false;
+        _aggroCoroutine = StartCoroutine(SwichFollowoffAfterCooldown());
+      }
+     
+    }
+
+    private void StopAggroCoroutine()
+    {
+      if (_aggroCoroutine != null)
+      {
+        StopCoroutine(_aggroCoroutine);
+        _aggroCoroutine = null;
+      }
+    }
+
+    private IEnumerator SwichFollowoffAfterCooldown()
+    {
+      yield return new WaitForSeconds(Cooldown);
+      SwitchFollowOff();
+    }
+
+    private void SwitchFollowOn() => 
+      Follow.enabled = true; 
+    
+    private void SwitchFollowOff() => 
+      Follow.enabled = false;
+    
+  }
+}
--- src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/Factory/GameFactory.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/Factory/GameFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using CodeBase.Infrastructure.AssetManagement;
 using CodeBase.Infrastructure.Services.PersistentProgress;
 using UnityEngine;
@@ -11,12 +12,18 @@ namespace CodeBase.Infrastructure.Factory
 
     public List<ISavedProgressReader> ProgressReaders { get; } = new List<ISavedProgressReader>();
     public List<ISavedProgress> ProgressWriters { get; } = new List<ISavedProgress>();
+    public GameObject HeroGameObject { get; set; }
+    public event Action HeroCreated;
 
     public GameFactory(IAssets assets) =>
       _assets = assets;
 
-    public GameObject CreateHero(GameObject at) =>
-      InstantiateRegistered(AssetPath.HeroPath, at.transform.position);
+    public GameObject CreateHero(GameObject at)
+    {
+      HeroGameObject = InstantiateRegistered(AssetPath.HeroPath, at.transform.position);
+      HeroCreated?.Invoke();
+      return HeroGameObject;
+    }
 
     public void CreateHud() =>
       InstantiateRegistered(AssetPath.HudPath);
