2734dadb14fff6399e1295179d449b1f5ccd3a5f
Artem <artembalandin13@gmail.com>
2021-07-25 18:26:15 +0300

    create attack component, and link it with animation

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Enemy/Attack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+  [RequireComponent(typeof(EnemyAnimator))]
+  public class Attack : MonoBehaviour
+  {
+    public EnemyAnimator Animator;
+
+    public float AttackCooldown = 3f;
+
+    private IGameFactory _factory;
+    private Transform _heroTransform;
+    private float _attackCooldown;
+    private bool _isAttacking;
+
+    private void Awake()
+    {
+      _factory = AllServices.Container.Single<IGameFactory>();
+      _factory.HeroCreated += OnHeroCreated;
+    }
+
+    private void Update()
+    {
+      UpdateCooldown();
+
+      if (CanAttack())
+        StartAttack();
+    }
+
+    private void OnAttack()
+    {
+    }
+
+    private void OnAttackEnded()
+    {
+      _attackCooldown = AttackCooldown;
+      _isAttacking = false;
+    }
+
+    private void StartAttack()
+    {
+      transform.LookAt(_heroTransform);
+      Animator.PlayAttack();
+
+      _isAttacking = true;
+    }
+
+    private void UpdateCooldown()
+    {
+      if (!CooldownIsUp())
+        _attackCooldown -= Time.deltaTime;
+    }
+
+    private void OnHeroCreated() =>
+      _heroTransform = _factory.HeroGameObject.transform;
+
+    private bool CanAttack() => 
+      !_isAttacking && CooldownIsUp();
+
+    private bool CooldownIsUp() => 
+      _attackCooldown <= 0;
+  }
+}
