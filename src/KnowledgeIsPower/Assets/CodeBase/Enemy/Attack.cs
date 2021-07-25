dbd0294b591f5742bcf28fa25d34952f6762d639
Artem <artembalandin13@gmail.com>
2021-07-25 18:45:05 +0300

    Add hitboxes

--- src/KnowledgeIsPower/Assets/CodeBase/Enemy/Attack.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Enemy/Attack.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using CodeBase.Infrastructure.Factory;
 using CodeBase.Infrastructure.Services;
 using UnityEngine;
@@ -11,16 +11,23 @@ namespace CodeBase.Enemy
     public EnemyAnimator Animator;
 
     public float AttackCooldown = 3f;
+    public float Cleavage = 0.5f;
+    public float EffectiveDistance = 0.5f;
 
     private IGameFactory _factory;
     private Transform _heroTransform;
     private float _attackCooldown;
     private bool _isAttacking;
+    private int _layerMask;
+    private Collider[] _hits = new Collider[1];
+
 
     private void Awake()
     {
       _factory = AllServices.Container.Single<IGameFactory>();
       _factory.HeroCreated += OnHeroCreated;
+
+      _layerMask = 1 << LayerMask.NameToLayer("Player");
     }
 
     private void Update()
@@ -33,6 +40,10 @@ namespace CodeBase.Enemy
 
     private void OnAttack()
     {
+      if (Hit(out Collider hit))
+      {
+        
+      }
     }
 
     private void OnAttackEnded()
@@ -41,12 +52,26 @@ namespace CodeBase.Enemy
       _isAttacking = false;
     }
 
+    private bool Hit(out Collider hit)
+    {
+      int hitsCount = Physics.OverlapSphereNonAlloc(StartPoint(), Cleavage, _hits, _layerMask);
+
+      hit = _hits.FirstOrDefault();
+      
+      return hitsCount > 0;
+    }
+
     private void StartAttack()
     {
       transform.LookAt(_heroTransform);
       Animator.PlayAttack();
 
       _isAttacking = true;
+    }
+
+    private Vector3 StartPoint()
+    {
+      return new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z) + transform.position * EffectiveDistance;
     }
 
     private void UpdateCooldown()
@@ -58,10 +83,10 @@ namespace CodeBase.Enemy
     private void OnHeroCreated() =>
       _heroTransform = _factory.HeroGameObject.transform;
 
-    private bool CanAttack() => 
+    private bool CanAttack() =>
       !_isAttacking && CooldownIsUp();
 
-    private bool CooldownIsUp() => 
+    private bool CooldownIsUp() =>
       _attackCooldown <= 0;
   }
 }
