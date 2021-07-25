4ab2d2394c75e81fbf2cfb675d0626aeba721abd
Artem <artembalandin13@gmail.com>
2021-07-25 22:56:06 +0300

    Create EnemyDeath

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Enemy/EnemyDeath.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+  [RequireComponent(typeof(EnemyHealth), typeof(EnemyAnimator))]
+  public class EnemyDeath : MonoBehaviour
+  {
+    public EnemyHealth Health;
+    public EnemyAnimator Animator;
+
+    public GameObject DeathFx;
+
+    public event Action Happened;
+
+    private void Start()
+    {
+      Health.HealthChanged += HealthChanged;
+    }
+
+    private void OnDestroy()
+    {
+      Health.HealthChanged -= HealthChanged;
+    }
+
+    private void HealthChanged()
+    {
+      if (Health.Current <= 0)
+        Die();
+    }
+
+    private void Die()
+    {
+      Health.HealthChanged -= HealthChanged;
+      
+      Animator.PlayDeath();
+
+      SpawnDeathFx();
+      StartCoroutine(DestroyTimer());
+      
+      Happened?.Invoke();
+    }
+
+    private void SpawnDeathFx() => 
+      Instantiate(DeathFx, transform.position, Quaternion.identity);
+
+    private IEnumerator DestroyTimer()
+    {
+      yield return new WaitForSeconds(3);
+      Destroy(gameObject);
+    }
+  }
+}
