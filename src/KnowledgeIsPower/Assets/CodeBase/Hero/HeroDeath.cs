aaff6cfa5f276fb649eb58c0aef158b2719e2671
Artem <artembalandin13@gmail.com>
2021-07-25 20:35:47 +0300

    create heroDeath component

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Hero/HeroDeath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Hero
+{
+  [RequireComponent(typeof(HeroHealth))]
+  public class HeroDeath : MonoBehaviour
+  {
+    public HeroHealth Health;
+
+    public HeroMove Move;
+    public HeroAnimator Animator;
+
+    public GameObject DeathFx;
+    private bool _isDeath;
+
+    private void Start() => 
+      Health.HealthChanged += HealthChanged;
+
+    private void OnDestroy() => 
+      Health.HealthChanged -= HealthChanged;
+
+    private void HealthChanged()
+    {
+      if (!_isDeath && Health.Current <= 0)
+        Die();
+    }
+
+    private void Die()
+    {
+      _isDeath = true;
+      
+      Move.enabled = false;
+      Animator.PlayDeath();
+
+      Instantiate(DeathFx, transform.position, Quaternion.identity);
+    }
+  }
+}
