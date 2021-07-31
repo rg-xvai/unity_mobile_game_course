31e85bc794cf5d387aa790e07c9ae077d3f71c17
Artem <artembalandin13@gmail.com>
2021-07-31 18:58:47 +0300

    create spawner prefab, add generate unique id to id, add it to editor

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Logic/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CodeBase.Data;
+using CodeBase.Infrastructure.Services.PersistentProgress;
+using CodeBase.StaticData;
+using UnityEngine;
+
+namespace CodeBase.Logic
+{
+  public class EnemySpawner : MonoBehaviour, ISavedProgress
+  {
+    public MonsterTypeId MonsterTypeId;
+    public bool Slain;
+    
+    private string _id;
+
+    private void Awake()
+    {
+      _id = GetComponent<UniqueId>().Id;
+    }
+
+    public void LoadProgress(PlayerProgress progress)
+    {
+      if (progress.KillData.ClearedSpawners.Contains(_id))
+        Slain = true;
+      else
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+    }
+
+    public void UpdateProgress(PlayerProgress progress)
+    {
+      if (Slain)
+        progress.KillData.ClearedSpawners.Add(_id);
+    }
+  }
+}
