5e9002d490e51aaca2e0ed36cb9fac6141ff22a6
Artem <artembalandin13@gmail.com>
2021-07-31 18:59:01 +0300

    add kill data to player progress

--- src/KnowledgeIsPower/Assets/CodeBase/Data/PlayerProgress.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Data/PlayerProgress.cs
@@ -9,12 +9,15 @@ namespace CodeBase.Data
     public State HeroState;
     public WorldData WorldData;
     public Stats HeroStats;
+    public KillData KillData;
+
 
     public PlayerProgress(string initialLevel)
     {
       WorldData = new WorldData(initialLevel);
       HeroState = new State();
       HeroStats = new Stats();
+      KillData = new KillData();
     }
   }
 }
