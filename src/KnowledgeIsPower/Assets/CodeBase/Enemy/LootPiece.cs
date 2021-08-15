b5fa92c14ed8f15ea6ec11fea39c675f70a6cf7c
Artem <artembalandin13@gmail.com>
2021-08-15 19:22:01 +0300

    Add pickup effects to skull item

--- src/KnowledgeIsPower/Assets/CodeBase/Enemy/LootPiece.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Enemy/LootPiece.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using CodeBase.Data;
+using TMPro;
 using UnityEngine;
 
 namespace CodeBase.Enemy
 {
   public class LootPiece : MonoBehaviour
   {
+    public GameObject Skull;
+    public GameObject PickupFxPrefab;
+    public TextMeshPro LootText;
+    public GameObject PickupPopup;
+    
     private Loot _loot;
     private bool _picked;
     private WorldData _worldData;
@@ -28,7 +36,32 @@ namespace CodeBase.Enemy
       
       _picked = true;
 
+      UpdateWorldData();
+      HideSkull();
+      PlayPickupFx();
+      ShowText();
+      StartCoroutine(StartDestroyTimer());
+    }
+
+    private void UpdateWorldData() => 
       _worldData.LootData.Collect(_loot);
+
+    private void HideSkull() => 
+      Skull.SetActive(false);
+
+    private IEnumerator StartDestroyTimer()
+    {
+      yield return new WaitForSeconds(1.5f);
+      Destroy(gameObject);
+    }
+
+    private void PlayPickupFx() => 
+      Instantiate(PickupFxPrefab, transform.position, Quaternion.identity);
+
+    private void ShowText()
+    {
+      LootText.text = $"{_loot.Value}";
+      PickupPopup.SetActive(true);
     }
   }
 }
