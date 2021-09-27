bb52d0ed652ad4ff387521ba701a7e8453843cac
roman_gorovtsov <feeling.digital.edu@gmail.com>
2021-09-27 19:36:54 +0300

    Added OnInitialized

--- src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/Services/IAP/IAAProvider.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/Services/IAP/IAAProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using CodeBase.Data;
 using UnityEditor;
 using UnityEngine;
@@ -12,28 +13,37 @@ namespace CodeBase.Infrastructure.Services.IAP
   public class IAAProvider : IStoreListener
   {
     private const string IAPCOnfigPath = "IAP/products";
+    private IStoreController _controller;
+    private IExtensionProvider _extensions;
+    
     private List<ProductConfig> _configs;
+
+    public event Action Initialized;
+    public bool IsInitialized => _controller != null && _extensions != null;
 
     public void Initialize()
     {
-      ConfigurationBuilder builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
       Load();
-      
+
+      ConfigurationBuilder builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+     
       foreach (ProductConfig productConfig in _configs) 
         builder.AddProduct(productConfig.Id, productConfig.Type);
       
       UnityPurchasing.Initialize( this, builder);
     }
 
-    private void Load()
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
     {
-      _configs = Resources.Load<TextAsset>(IAPCOnfigPath).text.ToDeserialized<ProductConfigWrapper>().Configs;
+      _controller = controller;
+      _extensions = extensions;
+
+      Initialized?.Invoke();
+      Debug.Log("UnityPurchasing initialization success");
     }
 
-    public void OnInitializeFailed(InitializationFailureReason error)
-    {
-      throw new System.NotImplementedException();
-    }
+    public void OnInitializeFailed(InitializationFailureReason error) => 
+      Debug.Log($"UnityPurchasing OnInitializeFailed:{error}");
 
     public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs purchaseEvent)
     {
@@ -45,9 +55,7 @@ namespace CodeBase.Infrastructure.Services.IAP
       throw new System.NotImplementedException();
     }
 
-    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
-    {
-      throw new System.NotImplementedException();
-    }
+    private void Load() => 
+      _configs = Resources.Load<TextAsset>(IAPCOnfigPath).text.ToDeserialized<ProductConfigWrapper>().Configs;
   }
 }
